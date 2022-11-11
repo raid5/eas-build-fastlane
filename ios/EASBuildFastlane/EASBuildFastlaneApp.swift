4a2807a03baf2bf99966312091086ca73bb24471
Adam McDonald <mcdonald.adam@gmail.com>
2022-11-11 13:57:29 -0800

    Initial commit

---
+++ ios/EASBuildFastlane/EASBuildFastlaneApp.swift
@@ -0,0 +1,17 @@
+//
+//  EASBuildFastlaneApp.swift
+//  EASBuildFastlane
+//
+//  Created by Adam McDonald on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct EASBuildFastlaneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
