10af3e927841bb1e1a6a8f3388b6a6e47016977a
Adam McDonald <mcdonald.adam@gmail.com>
2022-11-14 13:44:30 -0800

    Run fastlane tests

--- ios/EASBuildFastlaneTests/EASBuildFastlaneTests.swift
+++ ios/EASBuildFastlaneTests/EASBuildFastlaneTests.swift
@@ -9,28 +9,21 @@ import XCTest
 @testable import EASBuildFastlane
 
 final class EASBuildFastlaneTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    func testExample1() throws {
+        let actual = true
+        let expected = true
+        XCTAssertEqual(actual, expected)
     }
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    func testExample2() throws {
+        let actual = false
+        let expected = true
+        XCTAssertEqual(actual, expected)
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testExample3() throws {
+        let actual = true
+        let expected = true
+        XCTAssertEqual(actual, expected)
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
